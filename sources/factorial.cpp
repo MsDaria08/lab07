afb61545567000b3a348c54fd34b133552290c25
MsDaria08 <kotovadasha08@gmail.com>
2022-04-29 11:51:45 +0300

    add changes

--- sources/factorial.cpp
+++ sources/factorial.cpp
@@ -1,13 +1,13 @@
 #include <factorial.h>
 
 int factorial (int num){
-    int res = 1;
-    if (num == 1 || num == 0) return res;
+    int result = 1;
+    if (num == 1 || num == 0) return result;
     else {
         for (int i = 1; i <= num; ++i) {
-            res *= i;
+            result *= i;
         }
     }
-    return res;
+    return result;
 }
 
